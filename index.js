84ae07ed3d3154e1a7222f3a27315cdd69b177af
wittawatTon <wittawat.naranong@gmail.com>
2024-07-28 09:59:51 +0700

    test pass

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+const multer = require('multer');
+const path = require('path');
 
 var app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
+
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/')
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + path.extname(file.originalname)) // Append the file extension
+  }
+});
+
+const upload = multer({ storage: storage });
+
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  if (!req.file) {
+    return res.status(400).json({ error: 'No file uploaded' });
+  }
+
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  });
+});
+
+
+
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
